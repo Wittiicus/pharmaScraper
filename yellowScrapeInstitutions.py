9ce1a79d221cdec12b30aca3d1e036c9766eca34
Wittiicus <adrixdewit@gmail.com>
2022-06-26 13:35:27 -0400

    Made Changes to original code for multiple phone numbers

--- yellowScrapeInstitutions.py
+++ yellowScrapeInstitutions.py
@@ -1,5 +1,6 @@
 from curses.ascii import BS
 from email import header
+from socketserver import ForkingMixIn
 from termios import BS0
 from bs4 import BeautifulSoup
 import urllib.request, requests 
@@ -22,21 +23,18 @@ with urllib.request.urlopen(url) as response:
 
 soup = BeautifulSoup(html, 'html.parser')
 
-articles = soup.find_all('div', class_ = 'listing__title--wrap')
-Links = soup.find_all('li', class_ = 'mlr__submenu__item')
+articles = soup.find_all('div', class_ = 'listing listing--bottomcta')
 
+for item in articles:
+    phoneNumber = item.find_all('li', class_ = 'mlr__submenu__item')
+    for items in phoneNumber:
+        pNumberData.append(items.text.replace('\n',''))
+    companyName = item.find('div', class_ = 'listing__title--wrap').text
+    streetAddress = item.find('span', {'itemprop': 'streetAddress'}).text.strip()
 
-for pnums in Links:
-    pNumberData.append(pnums.text.strip())
+    print(companyName, pNumberData, streetAddress)
+    pNumberData = []
 
-
-for items in articles:
-    nameData.append(items.text.strip())
-
-print(len(nameData), len(pNumberData))
-
-df = pd.DataFrame({'Pharmacy Name' : nameData, 'Pharmacy Website' : pNumberData})
-df.columns = ['Pharmacy Name', 'Pharmacy Website']
 
 
 writer = pd.ExcelWriter('demo.xlsx', engine='xlsxwriter')
