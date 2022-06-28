d4840863e66750e7a57f5887a975018fc34312b3
Wittiicus <adrixdewit@gmail.com>
2022-06-27 22:58:45 -0400

    Refined code and added elemnts of encapsulation, added functionality that automatically finds the maximum number of page numbers

--- yellowScrapeInstitutions.py
+++ yellowScrapeInstitutions.py
@@ -8,7 +8,6 @@ import pandas as pd
 from tabulate import tabulate
 
 nameData = []
-pNumberData = []
 companyName = []
 streetAddress = []
 postalCode = []
@@ -24,48 +23,66 @@ headers = {'Accept-Encoding' : 'gzip, deflate, br',
             'Host' : 'www.yellowpages.com',
             }
 
-html = requests.get('https://www.yellowpages.ca/search/si/1/Pharmacies/Canada', headers=headers)
+def scrape(url):
+    html = requests.get(url, headers=headers)
 
-with urllib.request.urlopen(url) as response:
-    html = response.read()
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
 
-soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser')
 
-articles = soup.find_all('div', class_ = 'listing listing--bottomcta')
-webby = soup.find_all('li', class_ = 'mlr__item mlr__item--website')
-#for i in webby:
-    #print(i.find('a').get('href'))
+    return soup.find_all('div', class_ = 'listing listing--bottomcta')
+    #webby = soup.find_all('li', class_ = 'mlr__item mlr__item--website')
+    #for i in webby:
+        #print(i.find('a').get('href'))
 
-       
-
-for item in articles:
-
-    phoneNumber = item.find_all('li', class_ = 'mlr__submenu__item')
-    for items in phoneNumber:
-        pNumberData.append(items.text.replace('\n',''))
-    numbersForCompany.append(pNumberData)
-    try:
-        companyName.append(item.find('div', class_ = 'listing__title--wrap').text)
-    except:
-        companyName.append('Unavailable')
-    try:
-        streetAddress.append(item.find('span', {'itemprop': 'streetAddress'}).text.strip())
-    except:
-        streetAddress.append('Unavailable')
-    try:
-        postalCode.append(item.find('span', {'itemprop': 'postalCode'}).text.strip())
-    except:
-        postalCode.append('Unavailable')
-    try:
-        city.append(item.find('span', {'itemprop': 'addressLocality'}).text.strip())
-    except:
-        city.append('Unavailable')
-    try: 
-        province.append(item.find('span', {'itemprop': 'addressRegion'}).text.strip())
-    except:
-        province.append('Unavailable')
+def extractData(articles):
     pNumberData = []
+    for item in articles:
 
+        phoneNumber = item.find_all('li', class_ = 'mlr__submenu__item')
+        for items in phoneNumber:
+            pNumberData.append(items.text.replace('\n',''))
+        numbersForCompany.append(pNumberData)
+        try:
+            companyName.append(item.find('div', class_ = 'listing__title--wrap').text)
+        except:
+            companyName.append('Unavailable')
+        try:
+            streetAddress.append(item.find('span', {'itemprop': 'streetAddress'}).text.strip())
+        except:
+            streetAddress.append('Unavailable')
+        try:
+            postalCode.append(item.find('span', {'itemprop': 'postalCode'}).text.strip())
+        except:
+            postalCode.append('Unavailable')
+        try:
+            city.append(item.find('span', {'itemprop': 'addressLocality'}).text.strip())
+        except:
+            city.append('Unavailable')
+        try: 
+            province.append(item.find('span', {'itemprop': 'addressRegion'}).text.strip())
+        except:
+            province.append('Unavailable')
+        pNumberData = []
+
+
+def getNumberOfPages(url):
+    html = requests.get(url, headers=headers)
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+    soup = BeautifulSoup(html, 'html.parser')
+    pageInfo = soup.find_all('span', class_ = "pageCount")
+    for i in pageInfo:
+        textData = i.find('span', class_= '').text
+    return textData.strip()
+
+
+print(getNumberOfPages(url))
+
+
+rawData = scrape(url)
+extractData(rawData)
 
 dict = {'Company Name': companyName,
         'Street Address':streetAddress,
@@ -78,7 +95,7 @@ dict = {'Company Name': companyName,
 df = pd.DataFrame(dict)
   
 # displaying the DataFrame
-print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
+#print(tabulate(df, headers = 'keys', tablefmt = 'psql'))
 
 
 writer = pd.ExcelWriter('demo.xlsx', engine='xlsxwriter')
